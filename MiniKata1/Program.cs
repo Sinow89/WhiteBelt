6d516aadcc3c3a45e2d4666667fb00f64d840db9
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-11-04 11:55:56 +0100

    MiniKata

--- MiniKata1/Program.cs
+++ MiniKata1/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int playerHealth = 100;
+        string playerName = "Arin";
+        float attackPower = 15.5f;
+        bool isParalyzed = true;
+        double goldCoins = 24.75;
+
+        
+        Console.WriteLine($"Character Name: {playerName}");
+        Console.WriteLine($"Health Points: {playerHealth}");
+        Console.WriteLine($"Attack Power: {attackPower}");
+        Console.WriteLine($"Is Paralyzed: {isParalyzed}");
+        Console.WriteLine($"Gold Coins: {goldCoins}");
+
     }
 }
