d3cc1959834dd018acbe39b5be5ae858a28eb763
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-11-05 16:25:43 +0100

    Kata4

---
+++ Kata4/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Kata4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        {
+            int numberOfEnemies = 1;
+            for (int numberOfWaves = 1; numberOfWaves < 3; numberOfWaves++)
+            {
+                Console.WriteLine($"Wave {numberOfWaves} is starting!");
+                for (int i = 0; i < 5; i++)
+            
+                {
+                    Console.WriteLine($"Enemy #{numberOfEnemies} spawned!");
+                    ++numberOfEnemies;
+                }
+                Console.WriteLine($"Wave {numberOfWaves} completed. Preparing for the next wave...");
+                numberOfEnemies = 1;
+                Console.WriteLine("");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("All waves completed! Victory is yours!");
+        }
+        
+
+    }
+}
